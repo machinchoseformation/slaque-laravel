9cda7c273bd45d837205467c1fe943cde42e6d90
Guillaume <machinchoseformation@gmail.com>
2018-11-21 17:30:13 +0100

    db seeds in progress

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $faker = Faker\Factory::create();
+
+        $usernames = ['yo', 'bla', 'pif', 'paf', 'pouf', 'toto', 'foo', 'bar'];
+
+        foreach($usernames as $username) {
+            DB::table('users')->insert([
+                "name" => $username,
+                "email"=> "$username@gmail.com",
+                "password"=> bcrypt($username),
+                "created_at" => $faker->dateTimeBetween('- 2 years')
+            ]);
+        }
+        //@todo fix this shit
+        for($i=0; $i<100; $i++){
+            DB::table('groups')->insert(
+                [
+                    "name" => $faker->words(3, true),
+                    "creator_id" => array_rand($usernames),
+                    "is_one_on_one" => $faker->boolean()
+                ]
+
+            );
+        }
     }
 }
